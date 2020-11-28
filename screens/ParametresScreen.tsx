47748234d17167f71f8e7b89e1f6e111ad22018e
Sven <glaglasven@live.com>
2020-11-28 12:07:13 +0100

    Dropdown to select avatar

--- screens/ParametresScreen.tsx
+++ screens/ParametresScreen.tsx
@@ -1,25 +1,82 @@
 import * as React from 'react';
+import { useState } from 'react';
 import { StyleSheet, Switch, Button, Alert} from 'react-native';
+import { AsyncStorage } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { SettingsScreen, SettingsData, Chevron  } from "react-native-settings-screen"
+import DropDownPicker from 'react-native-dropdown-picker';
 
 export default function ParametresScreen() {
+  const _retrieveDataAvatar = async () => {
+    try {
+      const value = await AsyncStorage.getItem('avatar_id');
+      console.debug(value)
+      if (value !== null) {
+        // We have data!!
+        return value;
+      }
+    } catch (error) {
+      console.error(error)
+      return null
+    }
+    return null
+  };
+
+  const [value, setValue] = useState('usa');
+  let get_av_id = _retrieveDataAvatar();
+  if(get_av_id != null) {
+    console.log(get_av_id)
+    setValue(get_av_id);
+  }
+  console.debug(value)
+  const _storeDataAvatar = async (id) => {
+    setValue(id)
+    try {
+      await AsyncStorage.setItem(
+        'avatar_id',
+        id
+      );
+    } catch (error) {
+      console.error(error)
+    }
+  };
+
+  const avatarPicker = <DropDownPicker
+    items={[
+        {label: 'USA', value: 'usa'},
+        {label: 'UK', value: 'uk'},
+        {label: 'France', value: 'france'},
+    ]}
+    defaultValue={value}
+    containerStyle={{height: 40, width: 200}}
+    onChangeItem={item => _storeDataAvatar(item.value)}
+    placeholder='Choisi un avatar'
+    style={{backgroundColor: '#fafafa'}}
+    itemStyle={{
+        justifyContent: 'flex-start'
+    }}
+    dropDownStyle={{backgroundColor: '#fafafa'}}
+  />
+
   const data: SettingsData = [
     {
       type: 'SECTION',
-      header: 'Paramètres principaux'.toUpperCase(),
+      header: 'Paramètres généraux'.toUpperCase(),
       footer: 'd-harcelement-app - REACT Lab',
       rows: [
         {
-          title: 'hello',
-          showDisclosureIndicator: true
-        },
-        {
-          title: 's',
-          renderAccessory: () => <Switch value onValueChange={() => {}} />,
-        },
+          title: 'Avatar',
+          renderAccessory: () => avatarPicker,
+        }
+      ]
+    },
+    {
+      type: 'SECTION',
+      header: 'Gestion des données'.toUpperCase(),
+      footer: 'd-harcelement-app - REACT Lab',
+      rows: [
         {
           title: 'Supprimer les données',
           titleStyle: {
