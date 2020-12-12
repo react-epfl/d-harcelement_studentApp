dc035c502336d643b34b1b26696e31bd2aa7bfd4
Sven Borden <glaglasven@live.com>
2020-12-12 13:51:05 +0100

    Clear storage space when cliking settings clear button

--- screens/ParametresScreen.tsx
+++ screens/ParametresScreen.tsx
@@ -1,17 +1,25 @@
 import * as React from 'react';
 import { useState } from 'react';
 import { StyleSheet, Switch, Button, Alert} from 'react-native';
-import { AsyncStorage } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { SettingsScreen, SettingsData, Chevron  } from "react-native-settings-screen"
 import DropDownPicker from 'react-native-dropdown-picker';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function ParametresScreen() {
   
   
+  const clearAsyncStorage = async() => {
+    try{
+      await AsyncStorage.removeItem('@school_id')
+    } catch(e){
+      console.error(e);
+    }
 
+    Alert.alert('Données supprimées')
+  }
 
   const data: SettingsData = [
     {
@@ -23,7 +31,7 @@ export default function ParametresScreen() {
           titleStyle: {
             color: 'red',
           },
-          renderAccessory: () => <Button title='Supprimer' color='#F00' onPress={() => Alert.alert('Données supprimées')} />
+          renderAccessory: () => <Button title='Supprimer' color='#F00' onPress={clearAsyncStorage} />
         }
       ]
     },
